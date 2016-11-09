d89d1ab63ada98b24e5a8a820e70f3d4cc61e58d
Ashley <ashleykwtam@gmail.com>
2016-11-09 12:27:18 -0500

    json data to excel file

--- spotipy/tracks.py
+++ spotipy/tracks.py
@@ -5,6 +5,7 @@ import spotipy
 import time
 import sys
 import pandas as pd
+import xlwt
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -12,6 +13,7 @@ sp.trace=False
 
 songs_array = pd.read_excel('songs.xlsx')
 print(songs_array)
+
 
 for index, row in songs_array.iterrows():
 	track_name = (row['songs'])
@@ -24,8 +26,16 @@ for index, row in songs_array.iterrows():
 	start = time.time()
 	features = sp.audio_features(tids)
 	delta = time.time() - start
-	print(json.dumps(features, indent=4))
+	data = json.dumps(features, indent=4)
 	print ("features retrieved in %.2f seconds" % (delta,))
+
+	wb = xlwt.Workbook()
+	ws = wb.add_sheet("test")
+	for i, row in enumerate(data):
+		for j, col in enumerate(row):
+			ws.write(i, j, col)
+	wb.save("sample.xls")
+
 
 # if len(sys.argv) > 1:
 #     track_name = ''.join(sys.argv[1:])
