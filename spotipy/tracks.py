416ea49cbbdec15dd9ad5b0540bbc00a9057c22c
Shikhar Gaur <shikhar.gaur.13@gmail.com>
2016-11-09 12:20:57 -0500

    outputting audio features into excel

--- spotipy/tracks.py
+++ spotipy/tracks.py
@@ -13,6 +13,9 @@ sp.trace=False
 songs_array = pd.read_excel('songs.xlsx')
 print(songs_array)
 
+jsonDict = {}
+song_name = ''
+
 for index, row in songs_array.iterrows():
 	track_name = (row['songs'])
 	results = sp.search(q=track_name, limit=1)
@@ -20,12 +23,19 @@ for index, row in songs_array.iterrows():
 	for i, t in enumerate(results['tracks']['items']):
 		print(' ', i, t['name'])
 		tids.append(t['uri'])
-
+		song_name = t['name']
+	
 	start = time.time()
 	features = sp.audio_features(tids)
+	jsonDict[index] = [song_name, features]
 	delta = time.time() - start
 	print(json.dumps(features, indent=4))
 	print ("features retrieved in %.2f seconds" % (delta,))
+df = pd.DataFrame(jsonDict).T
+writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
+writer.save()
+
 
 # if len(sys.argv) > 1:
 #     track_name = ''.join(sys.argv[1:])
