49c9f9628e8f523e8af8166d48f583f98289cca0
sunnyshikhar <shikhar.gaur.13@gmail.com>
2016-11-16 22:42:40 -0500

    added popularity of track. Added functionality to split attributes into excel columns

--- spotipy/tracks.py
+++ spotipy/tracks.py
@@ -21,16 +21,38 @@ for index, row in songs_array.iterrows():
 	track_name = (row['songs'])
 	results = sp.search(q=track_name, limit=1)
 	tids = []
-	for i, t in enumerate(results['tracks']['items']):
-		print(' ', i, t['name'])
-		tids.append(t['uri'])
-		song_name = t['name']
+
+	if not results['tracks']['items']:
+		song_name = track_name
+		jsonDict[index] = [song_name, '', '', '', '', '', '', '', '']
+		print (song_name)
 	
-	features = sp.audio_features(tids)
-	jsonDict[index] = [song_name, features]
-	print(json.dumps(features, indent=4))
+	else:
+		for i, t in enumerate(results['tracks']['items']):
+			
+			#print(' ', i, t['name'], t['artists'], t['popularity'])
+			tids.append(t['uri'])
+			
+			#define each varialbe needed from the "search for an item" query for the project
+			song_name = t['name']
+			popularity = t['popularity']
+			artists = t['artists']		
+		
+		features = sp.audio_features(tids)
+		
+		#define each variable needed from the "get audio features" query for the project
+		energy = features[0]['energy']
+		dance = features[0]['danceability']
+		liveness = features[0]['liveness']
+		valence = features[0]['valence']
+		tempo = features[0]['tempo']
+		instrumental = features[0]['instrumentalness']
+		acoustic = features[0]['acousticness']
+		jsonDict[index] = [song_name, energy, dance, liveness, valence, tempo, instrumental, acoustic, popularity]
+		print(json.dumps(features, indent=4))
 
 df = pd.DataFrame(jsonDict).T
+df.columns = ['song', 'energy', 'dance', 'liveness', 'valence', 'tempo', 'instrumental', 'acoustic', 'popularity']
 writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
